ad34fd3842567c64412e8a75f2b0d18cb901176b
Uncannyjrevs <ragajervee@gmail.com>
2026-02-19 06:21:17 +0800

    initial commit

--- Midterm_API/Controllers/StudentController.cs
+++ Midterm_API/Controllers/StudentController.cs
@@ -21,23 +21,45 @@ namespace Midterm_API.Controllers
             return Ok(_studentService.GetAllStudents());
         }
 
+
+        [HttpGet]
+        public IActionResult Get(int id)
+        {
+            var Student = _studentService.GetSingleStudent(id);
+            return Student != null ? NotFound() : Ok(Student);
+        }
         //GET
         //Create GetSingle endpoint here with "id" as parameter
         //return OK if found, NotFound if not found
 
 
+        [HttpPost]
+        public IActionResult Post(int id,[FromBody] Student student)
+        {
+           
+            _studentService.AddStudent(student);
+            return Ok();
+        }
         //POST
         //Create AddStudent endpoint here
         //Accept Student object as parameter
 
-
-
+        [HttpPut]
+        public IActionResult Put(int id, [FromBody]Student student)
+        {
+            _studentService.UpdateStudent(student);
+            return Ok();
+        }
         //PUT
         //Create UpdateStudent endpoint here
         //Accept "id" as parameter and Student object as body
 
-
-
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            _studentService.DeleteStudent(id);
+            return Oks();
+        }
         //DELETE
         //Create DeleteStudent endpoint here
         //Accept "id" as parameter
