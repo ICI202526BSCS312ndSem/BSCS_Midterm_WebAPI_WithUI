a3945ee3f962a7084e45893f4223a25f890ec574
Shunsui Kyōraku <markrainerdionq@gmail.com>
2026-02-19 14:20:23 +0800

    Update StudentService.cs

--- Midterm_API/Services/StudentService.cs
+++ Midterm_API/Services/StudentService.cs
@@ -1,38 +1,59 @@
-﻿using Midterm_API.DAL;
+using Midterm_API.DAL;
 using Midterm_API.Entities;
 
 namespace Midterm_API.Services
 {
-    public class StudentService
-    {
-        private readonly StudentRepository _repository;
+	public class StudentService
+	{
+		private StudentRepository _repository;
 
-        public StudentService(StudentRepository repository)
-        {
-            _repository = repository;
-        }
+		public StudentService(StudentRepository repository)
+		{
+			_repository = repository;
+		}
 
-        public IEnumerable<Student> GetAllStudents()
-        {
-            return _repository.GetAll().OrderByDescending(p => p.Id);
-        }
+		public IEnumerable<Student> GetAllStudents()
+		{
+			var list = _repository.GetAll();
+			return list.OrderBy(x => x.Id);
+		}
 
-        //Create GetSingleStudent method here
-        //Accept "id" as parameter and return Student
+		//Create GetSingleStudent method here 
+		//Accept "id" as parameter and return Student
 
+		public Student GetSingleStudent(int id)
+		{
+			return _repository.GetById(id);
+		}
 
+		//Create AddStudent method here 
+		//Accept Student object as parameter and return void
 
-        //Create AddStudent method here
-        //Accept Student object as parameter and return void
+		public void AddStudent(Student student)
+		{
+			if (student != null)
+			{
+				_repository.Add(student);
+			}
+		}
 
+		//Create UpdateStudent method here 
+		//Accept Student object as parameter and return void
 
+		public void UpdateStudent(Student student)
+		{
+			if (student != null)
+			{
+				_repository.Update(student);
+			}
+		}
 
-        //Create UpdateStudent method here
-        //Accept Student object as parameter and return void
+		//Create DeleteStudent method here 
+		//Accept "id" as parameter and return void
 
-
-
-        //Create DeleteStudent method here
-        //Accept "id" as parameter and return void
-    }
+		public void DeleteStudent(int id)
+		{
+			_repository.Delete(id);
+		}
+	}
 }
