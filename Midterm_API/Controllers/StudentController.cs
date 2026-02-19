f2bb0b2439a3a8970cb59f82a46fb8545e3e2ec0
rico <kingandrei6405@gmail.com>
2026-02-18 22:21:43 -0800

    updated

--- Midterm_API/Controllers/StudentController.cs
+++ Midterm_API/Controllers/StudentController.cs
@@ -24,23 +24,44 @@ namespace Midterm_API.Controllers
         //GET
         //Create GetSingle endpoint here with "id" as parameter
         //return OK if found, NotFound if not found
-
+        [HttpGet("{id}[Controller]")]
+        public IActionResult Get(int id)
+        {
+            var Student = _studentService.GetSingleStudent(id);
+            return Student != null ? NotFound(): Ok(Student);
+        }
 
         //POST
         //Create AddStudent endpoint here
         //Accept Student object as parameter
-
+        [HttpPost]
+        public IActionResult Post(Student student)
+        {
+            _studentService.AddStudent(student);
+            return Ok();
+        }
 
 
         //PUT
         //Create UpdateStudent endpoint here
         //Accept "id" as parameter and Student object as body
-
+        [HttpPut("{id}[Controller]")]
+        public IActionResult Put(int id,[FromBody] Student student)
+        {
+            student.Id = id;
+            _studentService.UpdatedStudent(student);
+            return Ok();
+        }
 
 
         //DELETE
         //Create DeleteStudent endpoint here
         //Accept "id" as parameter
-       
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            _studentService.DeleteStudent(id);
+            return Ok();
+        }
     }
 }
