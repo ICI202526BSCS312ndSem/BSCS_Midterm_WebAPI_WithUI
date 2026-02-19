2b6fefa029a6f4ff1dde3d07f81be0bb395756ec
life4seph <mjflores039@gmail.com>
2026-02-19 14:22:11 +0800

    Midterm

--- Midterm_API/Controllers/StudentController.cs
+++ Midterm_API/Controllers/StudentController.cs
@@ -24,23 +24,78 @@ namespace Midterm_API.Controllers
         //GET
         //Create GetSingle endpoint here with "id" as parameter
         //return OK if found, NotFound if not found
-
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var existingStudent = _studentService.GetSingleStudent(id);
+            if (existingStudent != null)
+            {
+                return StatusCode(200, _studentService);
+            }
+            else
+            {
+                return StatusCode(404);
+            }
+        }
 
         //POST
         //Create AddStudent endpoint here
         //Accept Student object as parameter
+        [HttpPost]
+        public IActionResult Post([FromBody]Student student)
+        {
+            if(student == null)
+            {
+                return StatusCode(400);
+            }
+
+            _studentService.CreateStudent(student);
+            return StatusCode(201);
+        }
 
 
 
         //PUT
         //Create UpdateStudent endpoint here
         //Accept "id" as parameter and Student object as body
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Student student)
+        {
+            student.Id = id;
+            if (student == null)
+            {
+                return StatusCode(404);
+            }
+            if (student.Section <= -1 || student.Section <= -1)
+            {
+                return StatusCode(400);
+            }
+            else
+            {
+                _studentService.UpdateStudent(student);
+                return StatusCode(200);
+            }
+        }
 
 
 
         //DELETE
         //Create DeleteStudent endpoint here
         //Accept "id" as parameter
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var existingStudent = _studentService.GetSingleStudent(id);
+            if(existingStudent != null)
+            {
+                _studentService.RemoveStudent(id);
+                return StatusCode(200);
+            }
+            else
+            {
+                return StatusCode(404);
+            }
+        }
        
     }
 }
