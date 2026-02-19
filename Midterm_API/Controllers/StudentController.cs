97a9636ea428f7a7f560907e9d41a1cced96e28c
chard <chardestavillo@gmail.com>
2026-02-18 22:01:58 -0800

    Implement CRUD endpoints in StudentController

--- Midterm_API/Controllers/StudentController.cs
+++ Midterm_API/Controllers/StudentController.cs
@@ -23,23 +23,48 @@ namespace Midterm_API.Controllers
 
         //GET
         //Create GetSingle endpoint here with "id" as parameter
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var student = (_studentService.GetSingleStudent(id));
+            return student == null ? NotFound() : Ok(student);
+        }
         //return OK if found, NotFound if not found
 
 
         //POST
         //Create AddStudent endpoint here
+        [HttpPost]
+        public IActionResult Post([FromBody] Student student)
+        {
+            _studentService.CreateStudent(student);
+            return Ok();
+        }
         //Accept Student object as parameter
 
 
 
         //PUT
         //Create UpdateStudent endpoint here
+        [HttpPut("{id}")]
+        public IActionResult Put(int id,[FromBody] Student student)
+        {
+            student.Id = id;
+           _studentService.UpdateStudent(student);
+            return Ok(id);
+        }
         //Accept "id" as parameter and Student object as body
 
 
 
         //DELETE
         //Create DeleteStudent endpoint here
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _studentService.RemoveStudent(id);
+            return Ok(id);
+        }
         //Accept "id" as parameter
        
     }
