1dffe276e896823a20d4271acea8b106e33db72e
Joseph Emmanuel, Fajardo <sephfajardo13@gmail.com>
2026-02-18 22:24:40 -0800

    ss

--- Midterm_API/Services/StudentService.cs
+++ Midterm_API/Services/StudentService.cs
@@ -18,19 +18,22 @@ namespace Midterm_API.Services
         }
 
         //Create GetSingleStudent method here
-        public Student GetSingleStudent(int id) => _repository.GetById(id);
         //Accept "id" as parameter and return Student
+        public Student GetSingleStudent(int id) => _repository.GetById(id);
+        
 
         //Create AddStudent method here
-        public void CreateStudent(Student student) => _repository.Add(student);
         //Accept Student object as parameter and return void
+        public void CreateStudent(Student student) => _repository.Add(student);
+       
 
         //Create UpdateStudent method here
-        public void UpdateStudent(Student student) => _repository.Update(student);
         //Accept Student object as parameter and return void
-
+        public void UpdateStudent(Student student) => _repository.Update(student);
+        
         //Create DeleteStudent method here
-        public void RemoveStudent(int id) => _repository.Delete(id);
         //Accept "id" as parameter and return void
+        public void RemoveStudent(int id) => _repository.Delete(id);
+        
     }
 }
