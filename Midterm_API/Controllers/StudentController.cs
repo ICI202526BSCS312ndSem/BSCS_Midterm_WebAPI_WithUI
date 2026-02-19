7d2a8c0cff02861e7ccfe160cc67de242b4a3d5b
krtbry <kurtragasa0257@gmail.com>
2026-02-19 15:50:44 +0900

    Update StudentController.cs

--- Midterm_API/Controllers/StudentController.cs
+++ Midterm_API/Controllers/StudentController.cs
@@ -38,7 +38,7 @@ namespace Midterm_API.Controllers
         public IActionResult Post([FromBody] Student student)
         {
             _studentService.CreateStudent(student);
-            return Ok();
+            return StatusCode(201);
         }
 
 
@@ -51,7 +51,7 @@ namespace Midterm_API.Controllers
         {
             student.Id = id;
             _studentService.UpdateStudent(student);
-            return Ok(student);
+            return StatusCode(204);
 
         }
 
