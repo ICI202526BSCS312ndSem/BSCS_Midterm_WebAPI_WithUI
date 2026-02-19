cb6e97c8d7ff8dd8d0056aef56f558daed338501
Kiyoshi1916 <pjjuelar2@gmail.com>
2026-02-18 22:39:07 -0800

    Midterm

--- Midterm_API/Controllers/StudentController.cs
+++ Midterm_API/Controllers/StudentController.cs
@@ -4,43 +4,68 @@ using Midterm_API.Services;
 
 namespace Midterm_API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class StudentController : ControllerBase
-    {
-        private readonly StudentService _studentService;
+	[Route("api/[controller]")]
+	[ApiController]
+	public class StudentController : ControllerBase
+	{
+		private readonly StudentService _studentService;
 
-        public StudentController(StudentService studentService)
-        {
-            _studentService = studentService;
-        }
+		public StudentController(StudentService studentService)
+		{
+			_studentService = studentService;
+		}
 
-        [HttpGet]
-        public IActionResult Get()
-        {
-            return Ok(_studentService.GetAllStudents());
-        }
+		[HttpGet]
+		public IActionResult Get()
+		{
+			return Ok(_studentService.GetAllStudents);
+		}
 
-        //GET
-        //Create GetSingle endpoint here with "id" as parameter
-        //return OK if found, NotFound if not found
+		//GET
+		//Create GetSingle endpoint here with "id" as parameter
+		//return OK if found, NotFound if not found
+		[HttpGet("{id}")]
+		public IActionResult Get(int id)
+		{
+			var student = (_studentService.GetSingleStudent(id));
+			return student == null ? NotFound() : Ok(student);
+		}
+
+		//POST
+		//Create AddStudent endpoint here
+		//Accept Student object as parameter
+		[HttpPost]
+		public IActionResult Post([FromBody] Student student)
+		{
+		
+
+			_studentService.CreateStudent(student);
+			return Ok();
+
+		}
 
 
-        //POST
-        //Create AddStudent endpoint here
-        //Accept Student object as parameter
+		//PUT
+		//Create UpdateStudent endpoint here
+		//Accept "id" as parameter and Student object as body
 
+		[HttpPut("{id}")]
+		public IActionResult Put(int id, [FromBody] Student student)
+		{
+			student.Id = id;
+			_studentService.UpdateStudent(student);
+			return Ok(id);
+		}
 
+		//DELETE
+		//Create DeleteStudent endpoint here
+		//Accept "id" as parameter
+		[HttpDelete("{id}")]
+		public IActionResult Delete(int id)
+		{
+			_studentService.RemoveStudent(id);
+			return Ok(id);
+		}
 
-        //PUT
-        //Create UpdateStudent endpoint here
-        //Accept "id" as parameter and Student object as body
-
-
-
-        //DELETE
-        //Create DeleteStudent endpoint here
-        //Accept "id" as parameter
-       
-    }
+	}
 }
--- Midterm_API/Services/StudentService.cs
+++ Midterm_API/Services/StudentService.cs
@@ -1,38 +1,41 @@
-﻿using Midterm_API.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Midterm_API.DAL;
 using Midterm_API.Entities;
 
 namespace Midterm_API.Services
 {
-    public class StudentService
-    {
-        private readonly StudentRepository _repository;
+	public class StudentService
+	{
+		private readonly StudentRepository _repository;
 
-        public StudentService(StudentRepository repository)
-        {
-            _repository = repository;
-        }
+		public StudentService(StudentRepository repository)
+		{
+			_repository = repository;
+		}
 
-        public IEnumerable<Student> GetAllStudents()
-        {
-            return _repository.GetAll().OrderByDescending(p => p.Id);
-        }
+		public IEnumerable<Student> GetAllStudents()
+		{
+			return _repository.GetAll().OrderByDescending(p => p.Id);
+		}
 
-        //Create GetSingleStudent method here
-        //Accept "id" as parameter and return Student
+		//Create GetSingleStudent method here
+		//Accept "id" as parameter and return Student
+		public Student GetSingleStudent(int id) => _repository.GetById(id);
 
 
 
-        //Create AddStudent method here
-        //Accept Student object as parameter and return void
-
+		//Create AddStudent method here
+		//Accept Student object as parameter and return void
+		public void CreateStudent(Student student) => _repository.Add(student);
 
 
         //Create UpdateStudent method here
         //Accept Student object as parameter and return void
+		public void UpdateStudent(Student student) => _repository.Update(student);
 
 
-
-        //Create DeleteStudent method here
-        //Accept "id" as parameter and return void
+		//Create DeleteStudent method here
+		//Accept "id" as parameter and return void
+		public void RemoveStudent(int id) => _repository.Delete(id);
     }
 }
