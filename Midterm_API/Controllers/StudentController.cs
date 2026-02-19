93ebd0e03bae6d397710018d33a848bc353c44a7
Shunsui Kyōraku <markrainerdionq@gmail.com>
2026-02-19 14:19:57 +0800

    Update StudentController.cs

--- Midterm_API/Controllers/StudentController.cs
+++ Midterm_API/Controllers/StudentController.cs
@@ -4,43 +4,85 @@ using Midterm_API.Services;
 
 namespace Midterm_API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class StudentController : ControllerBase
-    {
-        private readonly StudentService _studentService;
+	[Route("api/[controller]")]
+	[ApiController]
+	public class StudentController : ControllerBase
+	{
+		private StudentService _studentService;
 
-        public StudentController(StudentService studentService)
-        {
-            _studentService = studentService;
-        }
+		public StudentController(StudentService studentService)
+		{
+			_studentService = studentService;
+		}
 
-        [HttpGet]
-        public IActionResult Get()
-        {
-            return Ok(_studentService.GetAllStudents());
-        }
+		[HttpGet]
+		public IActionResult Get()
+		{
+			var students = _studentService.GetAllStudents();
+			return Ok(students);
+		}
 
-        //GET
-        //Create GetSingle endpoint here with "id" as parameter
-        //return OK if found, NotFound if not found
+		// GET
+		// Create GetSingle endpoint here with "id" as parameter
+		// return OK if found, NotFound if not found
 
+		[HttpGet("{id}")]
+		public IActionResult GetSingle(int id)
+		{
+			var student = _studentService.GetSingleStudent(id);
 
-        //POST
-        //Create AddStudent endpoint here
-        //Accept Student object as parameter
+			if (student == null)
+			{
+				return NotFound();
+			}
 
+			return Ok(student);
+		}
 
+		// POST
+		// Create AddStudent endpoint here
+		// Accept Student object as parameter
 
-        //PUT
-        //Create UpdateStudent endpoint here
-        //Accept "id" as parameter and Student object as body
+		[HttpPost]
+		public IActionResult AddStudent([FromBody] Student student)
+		{
+			if (student == null)
+			{
+				return BadRequest();
+			}
 
+			_studentService.AddStudent(student);
+			return Ok(student);
+		}
 
+		// PUT
+		// Create UpdateStudent endpoint here
+		// Accept "id" as parameter and student object as body
 
-        //DELETE
-        //Create DeleteStudent endpoint here
-        //Accept "id" as parameter
-       
-    }
+		[HttpPut("{id}")]
+		public IActionResult UpdateStudent(int id, [FromBody] Student student)
+		{
+			if (student == null)
+			{
+				return BadRequest();
+			}
+            
+			student.Id = id;   
+
+			_studentService.UpdateStudent(student);
+
+			return Ok();
+		}
+
+		// DELETE
+		// Create DeleteStudent endpoint here
+		// Accept "id" as parameter
+
+		[HttpDelete("{id}")]
+		public IActionResult DeleteStudent(int id)
+		{
+			_studentService.DeleteStudent(id);
+			return Ok();
+		}
+	}
 }
